4e2b0fb205abd5ee518c79cba6fa2c08e1618b57
Calliyope <erin.joosen@gmail.com>
2019-10-30 11:40:05 +0100

    Explained the js process and what I founds

--- 2.The-Hills/1.JavaScript-Basics/1.Read-Fix-Write/01-Read-It/03/js/script.js
+++ 2.The-Hills/1.JavaScript-Basics/1.Read-Fix-Write/01-Read-It/03/js/script.js
@@ -4,3 +4,23 @@ for (let i = 0; i < amount; i++) {
   console.log(i);
   document.write(`<p>${i + 1}</p>`);
 }
+
+
+//I predict that this is going to log 1-9 in the console and display 11 on the screen....
+
+//I was wrong about what would be displayed!
+
+// This created 10 paragraphs that appear one on top of the other. It logs each instance of the operation performed so it shows us 1-10
+
+//        1
+//        2
+//        3
+//        4
+//        5
+//        6
+//        7
+//        8
+//        9
+//        10
+
+// The console indeed did log 0-9. It checks if i is bigger than the amount (10) and logs it every time until the amount would be equal to the amount.
