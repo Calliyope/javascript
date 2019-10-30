0573f15f900c270294bbab661f022599557de3fd
Calliyope <erin.joosen@gmail.com>
2019-10-30 11:28:10 +0100

    Explored and commented my findings on the js file.

--- 2.The-Hills/1.JavaScript-Basics/1.Read-Fix-Write/01-Read-It/02/js/script.js
+++ 2.The-Hills/1.JavaScript-Basics/1.Read-Fix-Write/01-Read-It/02/js/script.js
@@ -1,5 +1,10 @@
+// This constant gives a variable - its name is 'amount' and the value of 'amount' is 20. 
 const amount = 20;
 
+//The document write creates text on the page, the use of amount means that the number 20 is inserted in the text
 document.write("<h1>Top " + amount + " Greatest Movies of All Time</h1>");
 
+//The same happens here except they have performed a mathamatical process on the amount and so it displays as 10 instead of the const value 20
 document.write(`<h2>The first ${amount / 2} movies</h2>`);
+
+
