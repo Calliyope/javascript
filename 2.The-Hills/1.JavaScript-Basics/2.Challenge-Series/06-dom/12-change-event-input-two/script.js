0821b108317637078c3f483335526da8e23cb4fc
Calliyope <erin.joosen@gmail.com>
2019-11-14 16:35:53 +0100

    a few more challenges

--- 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/06-dom/12-change-event-input-two/script.js
+++ 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/06-dom/12-change-event-input-two/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    document.getElementById("pass-one").addEventListener('keyup', function () {
+        let passInput = document.getElementById("pass-one").value;
 
+        let digitCount = passInput.replace(/[^0-9]/g, "").length;
+        console.log(digitCount);
+
+        if (passInput.length >= 8 &&
+            (digitCount) >= 2) {
+            console.log("ok");
+        } else {
+            console.log("not ok");
+        }
+    });
 })();
--- 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/07-misc/01-waving-text/script.js
+++ 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/07-misc/01-waving-text/script.js
@@ -9,8 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    let targetText = document.getElementById("target").textContent;
+
+    console.log(targetText);
+
+    let letters = targetText.charAt(60)
+
+    console.log(letters);
+
+    // I need to loop through the string of letters (targetText) using the charAt function and for each one set a different font size. The loop should ascend for 5 characters and descend for the next 5, repeating that process. The output should be wavy text. 
 
 })();
